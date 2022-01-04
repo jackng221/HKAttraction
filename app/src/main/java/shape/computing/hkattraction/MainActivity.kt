a5db7214be8a96712f23c5b7922872bc33622479
jackng221 <jackng1233@gmail.com>
2022-01-05 07:18:47 +0800

    Layouts#1
    main1: startup title, fingerprint TBC
    main2: main page, navigation bar TBC

--- app/src/main/java/shape/computing/hkattraction/MainActivity.kt
+++ app/src/main/java/shape/computing/hkattraction/MainActivity.kt
@@ -1,11 +1,23 @@
 package shape.computing.hkattraction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timer().schedule(3000){
+            enterApp()
+        }
+    }
+
+    private fun enterApp(){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/shape/computing/hkattraction/MainActivity2.kt
@@ -0,0 +1,13 @@
+package shape.computing.hkattraction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+}
