2bb5647b9e41eef10c1aafbb94fa23aa106ee6d2
jackng221 <jackng1233@gmail.com>
2022-01-12 03:15:47 +0800

    recyclerView#4
    +check custom image availability (deleted?), if not use default image

--- app/src/main/java/RecyclerAdapter.kt
+++ app/src/main/java/RecyclerAdapter.kt
@@ -1,8 +1,6 @@
 import android.content.Context
 import android.content.Intent
-import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +10,10 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import shape.computing.hkattraction.MapsActivity
 import shape.computing.hkattraction.R
+import java.io.InputStream
 
 
-class RecyclerAdapter (private val dbHelper: AttractionDbHelper, private val context: Context): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter(private val dbHelper: AttractionDbHelper, private val context: Context): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val imgButton: ImageButton = view.findViewById(R.id.imageButton)
@@ -37,8 +36,18 @@ class RecyclerAdapter (private val dbHelper: AttractionDbHelper, private val con
                 holder.imgButton.setImageDrawable(img)
             }
             else -> {
-                val uri = Uri.parse(dbHelper.getData(position + 1, AttractionDbHelper.AttractionEntry.COLUMN_NAME_CUSTOM_IMG_URI).toString())
+                try{
+                    val uri = Uri.parse(dbHelper.getData(position + 1, AttractionDbHelper.AttractionEntry.COLUMN_NAME_CUSTOM_IMG_URI).toString())
+                    // check if file exists
+                    val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+                    inputStream?.close()
                     holder.imgButton.setImageURI(uri)
+                }
+                catch (e: Exception){
+                    val name = dbHelper.getData(position + 1, AttractionDbHelper.AttractionEntry.COLUMN_NAME_DEFAULT_IMG).toString()
+                    val img = ResourcesCompat.getDrawable(context.resources, context.resources.getIdentifier(name, "drawable", context.packageName), null)
+                    holder.imgButton.setImageDrawable(img)
+                }
             }
         }
 
