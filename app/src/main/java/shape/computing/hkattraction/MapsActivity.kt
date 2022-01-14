54f5b0a21ac2b78e9d09a25a3503eba2b1478c17
jackng221 <jackng1233@gmail.com>
2022-01-14 19:41:30 +0800

    very minor cleaning

--- app/src/main/java/shape/computing/hkattraction/MapsActivity.kt
+++ app/src/main/java/shape/computing/hkattraction/MapsActivity.kt
@@ -24,7 +24,6 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import java.io.File
 import java.util.*
 
 
